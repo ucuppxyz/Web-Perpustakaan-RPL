89a0fe93f4a6c4ba0c7f04d3531bead5403fb6bd
Yusuf <ucupptegal@gmail.com>
2025-12-27 09:07:03 +0700

    Update lokasi

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -208,7 +208,7 @@ export function Layout({ user, onLogout, onNavigate, currentPage, children }: La
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                   </svg>
-                  <span>Jl. Pendidikan No. 123, Jakarta Selatan</span>
+                  <span>Jl. Dampyak NO.123, Dampyak selatan</span>
                 </li>
               </ul>
             </div>
